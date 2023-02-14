7e31cecd88bb24a57cfcec2228e66b31257c569f
Lazaros Poumpouridis <psyzed66@gmail.com>
2023-02-14 21:17:50 +0200

    some new routes were added

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,14 +1,93 @@
 const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 router.get(`/`, async (req, res) => {
-  const userList = await User.find();
+  const userList = await User.find().select("-passwordHash");
 
   if (!userList) {
     res.status(500).json({ success: false });
   }
   res.send(userList);
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) {
+      res.status(404).json({ success: false, message: "User not found!" });
+    } else {
+      res.status(200).json({ success: true, user: user });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong, please try again later!",
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const userToUpdate = await User.findById(req.params.id);
+    let newPassword;
+    if (req.body.password) {
+      newPassword = bcrypt.hashSync(req.body.password, 10);
+    } else {
+      newPassword = userToUpdate.password;
+    }
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        email: req.body.email,
+        password: newPassword,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+      },
+      { new: true }
+    );
+    if (!user) {
+      res.status(404).json({ success: false, message: "User not found" });
+    } else {
+      res
+        .status(200)
+        .json({ success: true, message: "User updated!", user: user });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Something went wrong!" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+  user = await user.save();
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ success: false, message: "User could not be created!" });
+  }
+  res.status(200).json({ user: user, message: "User Created!" });
 });
 
 module.exports = router;
